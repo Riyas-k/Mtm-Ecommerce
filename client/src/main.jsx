5c407bac3a190e733666a212346a29981939b3b6
Athif Binu <binuathif@gmail.com>
2023-09-13 12:16:26 +0530

    test

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import {BrowserRouter} from "react-router-dom"
-
+import "./styles/Common.css"
+import "./styles/index.css"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
